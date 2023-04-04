a70d7d8e88f16d5517177dff4414129c00c86d24
Pham Phuoc Dong <s3817726@rmit.edu.vn>
2023-04-04 12:06:36 +0700

    fix bug with pulling image from DockerHub

--- k8s/k8s-web-hello/index.mjs
+++ k8s/k8s-web-hello/index.mjs
@@ -2,15 +2,15 @@ import express from 'express';
 import os from 'os';
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
 app.get("/", (req, res) => {
   const helloMessage = `Hello from the ${os.hostname()}`;
   console.log(helloMessage);
-  res.send(helloMessage);
+  res.send(helloMessage); 
 });
 
 app.listen(PORT, () => {
-  console.log(`Web server is listening at port ${port}`);
+  console.log(`Web server is listening at port ${PORT}`);
 });
 
