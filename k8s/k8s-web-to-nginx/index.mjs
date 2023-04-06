c89a8bd07c680a16e65c7edfff03b8586a1e29af
Pham Phuoc Dong <s3817726@rmit.edu.vn>
2023-04-06 12:15:20 +0700

    lean Kubernetes: create svc and deploy with yaml

--- k8s/k8s-web-to-nginx/index.mjs
+++ k8s/k8s-web-to-nginx/index.mjs
@@ -1,13 +1,21 @@
 import express from 'express';
+import fetch from 'node-fetch';
 import os from 'os';
 
 const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  const helloMessage = `Version 2: Hello from the ${os.hostname()}`;
+  const helloMessage = `Hello from the ${os.hostname()}`;
   console.log(helloMessage);
   res.send(helloMessage); 
+});
+
+app.get("/nginx", async (req, res) => {
+  const url = 'http://nginx';
+  const response = await fetch(url);
+  const body = await response.text();
+  res.send(body);
 });
 
 app.listen(PORT, () => {
