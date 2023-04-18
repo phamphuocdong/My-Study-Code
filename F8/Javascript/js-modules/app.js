2791e9748597b2c1c7d99d0a24ef6d3ef20926b0
Pham Phuoc Dong <s3817726@rmit.edu.vn>
2023-04-18 14:57:26 +0700

    make tab ui, working on music player

---
+++ F8/Javascript/js-modules/app.js
@@ -0,0 +1,4 @@
+// import logger from './logger.js'
+import * as constants from './constants.js'
+
+console[constants.TYPE_ERROR]("hello");
---
+++ F8/Javascript/js-modules/constants.js
@@ -0,0 +1,5 @@
+const TYPE_LOG = 'log';
+const TYPE_WARN = 'warn';
+const TYPE_ERROR = 'error';
+
+export {TYPE_LOG , TYPE_WARN, TYPE_ERROR};
