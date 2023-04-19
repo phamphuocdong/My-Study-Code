d38c7a0f171115bb2ce41d18be7f33c525b56279
Pham Phuoc Dong <s3817726@rmit.edu.vn>
2023-04-19 14:58:31 +0700

    add function for music player btn

--- F8/Javascript/music-player/main.js
+++ F8/Javascript/music-player/main.js
@@ -1,6 +1,6 @@
 // 1. Render songs
 // 2. Scroll top
-// 3. Play / pause / seek
+// 3. Play / pause / seek    //? src: HTML Audio/Video methods
 // 4. CD rotate
 // 5. Next / Prev
 // 6. Random
@@ -8,23 +8,33 @@
 // 8. Active song
 // 9. Scroll active song into view
 // 10. Play song when click
+
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-const playlist = $('.playlist')
+const playlist = $('.playlist');
+const player = $('.player');
+const heading = $('header h2');
+const cdThumb = $('.cd-thumb');
+const audio = $('#audio');
+const cd = $('.cd');
+const playBtn = $('.btn-toggle-play');
+const progress = $('#progress');
 
 const app = {
+  currentIndex: 0,
+  isPlaying: false,
   songs: [
     {
       name: "Click Pow Get Down",
       singer: "Raftaar x Fortnite",
-      path: "https://mp3.vlcmusic.com/download.php?track_id=34737&format=320",
+      path: "./assets/music/song1.mp3",
       image: "https://i.ytimg.com/vi/jTLhQf5KJSc/maxresdefault.jpg"
     },
     {
       name: "Tu Phir Se Aana",
       singer: "Raftaar x Salim Merchant x Karma",
-      path: "https://mp3.vlcmusic.com/download.php?track_id=34213&format=320",
+      path: "./assets/music/song2.mp3",
       image:
         "https://1.bp.blogspot.com/-kX21dGUuTdM/X85ij1SBeEI/AAAAAAAAKK4/feboCtDKkls19cZw3glZWRdJ6J8alCm-gCNcBGAsYHQ/s16000/Tu%2BAana%2BPhir%2BSe%2BRap%2BSong%2BLyrics%2BBy%2BRaftaar.jpg"
     },
@@ -32,27 +42,27 @@ const app = {
       name: "Naachne Ka Shaunq",
       singer: "Raftaar x Brobha V",
       path:
-        "https://mp3.filmysongs.in/download.php?id=Naachne Ka Shaunq Raftaar Ft Brodha V Mp3 Hindi Song Filmysongs.co.mp3",
+        "./assets/music/song3.mp3",
       image: "https://i.ytimg.com/vi/QvswgfLDuPg/maxresdefault.jpg"
     },
     {
       name: "Mantoiyat",
       singer: "Raftaar x Nawazuddin Siddiqui",
-      path: "https://mp3.vlcmusic.com/download.php?track_id=14448&format=320",
+      path: "./assets/music/song1.mp3",
       image:
         "https://a10.gaanacdn.com/images/song/39/24225939/crop_480x480_1536749130.jpg"
     },
     {
       name: "Aage Chal",
       singer: "Raftaar",
-      path: "https://mp3.vlcmusic.com/download.php?track_id=25791&format=320",
+      path: "./assets/music/song2.mp3",
       image:
         "https://a10.gaanacdn.com/images/albums/72/3019572/crop_480x480_3019572.jpg"
     },
     {
       name: "Feeling You",
       singer: "Raftaar x Harjas",
-      path: "https://mp3.vlcmusic.com/download.php?track_id=27145&format=320",
+      path: "./assets/music/song3.mp3",
       image:
         "https://a10.gaanacdn.com/gn_img/albums/YoEWlabzXB/oEWlj5gYKz/size_xxl_1586752323.webp"
     }
@@ -77,22 +87,72 @@ const app = {
     $('.playlist').innerHTML = htmls.join('');
   },
 
+  defineProperties: function() {
+    Object.defineProperty(this, 'currentSong', {
+      get: function() {
+        return this.songs[this.currentIndex];
+      }
+    })
+  },
+
   handleEvents: function() {
-    const cd = $('.cd');
+    const _this = this;
     const cdWidth = cd.offsetWidth;
     
+    // Modify CD size
     document.onscroll = function() {
-      const scrollTop = window.scrollY || document.documentElement.scrollTop; //todo: video 13:43
+      const scrollTop = window.scrollY || document.documentElement.scrollTop;
       const newCdWith = cdWidth - scrollTop
 
-      console.log(newCdWith);
       cd.style.width = newCdWith > 0 ? newCdWith + 'px' : 0;
       cd.style.opacity = newCdWith / cdWidth; 
     }
+
+    // Click play
+    playBtn.onclick = function() {
+      if (_this.isPlaying) {
+        audio.pause();
+      } else {
+        audio.play();
+      }
+      
+    }
+
+    // When song is played
+    audio.onplay = function() {
+      _this.isPlaying = true;
+      player.classList.add('playing')
+    }
+
+    // When song is paused
+    audio.onpause = function() {
+      _this.isPlaying = false;
+      player.classList.remove('playing')
+    }
+
+    // When song progress changed
+    audio.ontimeupdate = function() {
+      //todo(video: 38:41): if(audio)
+      console.log(audio.currentTime / audio.duration * 100);
+    }
+  },
+
+  loadCurrentSong: function() {
+    heading.textContent = this.currentSong.name;
+    cdThumb.style.backgroundImage = `url('${this.currentSong.image}')`;
+    audio.src = this.currentSong.path;
   },
 
   start: function() {
-    this.handleEvents()
+    // Define properties for object
+    this.defineProperties();
+
+    //Listen to DOM events and handle it
+    this.handleEvents();
+
+    this.loadCurrentSong();
+
+    //Render playlist
     this.render();
 
 
